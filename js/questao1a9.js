981f550521e323667c237e1249304cf9363f366d
Guilherme Costa <167817674+cdGuilherme@users.noreply.github.com>
2026-06-10 23:45:48 -0300

    Atualização
    - Atualizado o CSS Reset para fazer o text-wrap padrão de headings
    - Atualizado a estilização de listas para ter uma margin menor
    - Adicionado a parte 1 das questões objetivas com respostas

---
+++ js/questao1a9.js
@@ -0,0 +1,67 @@
+"use strict";
+
+let respostas = [
+    "c) addEventListener()",
+    "b) GET",
+    "b) Fazer requisições HTTP",
+    "c) 200",
+    "b) Um único valor resultante",
+    "c) find()",
+    "b) Automaticamente",
+    "c) O restante dos elementos do array",
+    "c) await"
+];
+
+// Questão 1:
+function questao1() {
+    console.log(respostas[0]);
+    alert(respostas[0]);
+}
+
+// Questão 2:
+function questao2() {
+    console.log(respostas[1]);
+    alert(respostas[1]);
+}
+
+// Questão 3:
+function questao3() {
+    console.log(respostas[2]);
+    alert(respostas[2]);
+}
+
+// Questão 4:
+function questao4() {
+    console.log(respostas[3]);
+    alert(respostas[3]);
+}
+
+// Questão 5:
+function questao5() {
+    console.log(respostas[4]);
+    alert(respostas[4]);
+}
+
+// Questão 6:
+function questao6() {
+    console.log(respostas[5]);
+    alert(respostas[5]);
+}
+
+// Questão 7:
+function questao7() {
+    console.log(respostas[6]);
+    alert(respostas[6]);
+}
+
+// Questão 8:
+function questao8() {
+    console.log(respostas[7]);
+    alert(respostas[7]);
+}
+
+// Questão 9:
+function questao9() {
+    console.log(respostas[8]);
+    alert(respostas[8]);
+}
